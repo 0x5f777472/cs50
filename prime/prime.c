cc7a309c3fe567cf8f05c30c0bad413ee3b1759f
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-02-26 19:52:36 +0000

    Sun, Feb 26, 2023, 7:52 PM +00:00

--- prime/prime.c
+++ prime/prime.c
@@ -1,7 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
 
-bool prime(int number);
+void sieve(int max, bool primes[]);
 
 int main(void)
 {
@@ -19,24 +21,36 @@ int main(void)
     }
     while (min >= max);
 
+    bool primes[max + 1];
+    sieve(max, primes);
+
     for (int i = min; i <= max; i++)
     {
-        if (prime(i))
+        if (primes[i])
         {
             printf("%i\n", i);
         }
     }
 }
 
-bool prime(int number)
+void sieve(int max, bool primes[])
 {
-    if (number <= 1) {
-        return false;
+    for (int i = 0; i <= max; i++)
+    {
+        primes[i] = true;
     }
-    for (int i = 2; i * i < number; i++) {
-        if (number % i == 0) {
-            return false;
+    primes[0] = false;
+    primes[1] = false;
+
+    int limit = sqrt(max);
+    for (int i = 2; i <= limit; i++)
+    {
+        if (primes[i])
+        {
+            for (int j = i * i; j <= max; j += i)
+            {
+                primes[j] = false;
+            }
         }
     }
-    return true;
 }
