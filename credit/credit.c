556f81eff781db37f7417a574ee008f31484be74
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-07 17:12:10 +0000

    Mon, Aug 7, 2023, 6:12 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,10 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
-
+int count(long number);
 int main(void)
 {
     while (1) {
         long input = get_long("Number: ");
-        if input
+        int c = count(input);
+        printf("%d\n", c);
     }
+}
+int count(long number) {
+    for (int i = 0; number > 1; i++) {
+        number /= 10;
+        return i;
+    }
+    return 0;
 }
