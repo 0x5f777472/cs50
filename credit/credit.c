842ea870943a813a9214be1cb1c02c4d7023755d
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-07 17:44:59 +0000

    Mon, Aug 7, 2023, 6:44 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,16 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 int count(long number);
+int luhn(long number);
 int main(void)
 {
     while (1) {
         long input = get_long("Number: ");
         int length = count(input);
-        if (length == 13 || length == 16) {
-            
+        if (luhn(input) % 10 == 0) {
+            if (length == 13)
         }
     }
 }
+
+int luhn(long number) {
+
+    return 0;
+}
+
 int count(long number) {
     int count = 1;
     while (number > 9) {
