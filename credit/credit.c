2a38382eb5fa9fe2c8ba53b0e37d095d46037892
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-07 17:58:00 +0000

    Mon, Aug 7, 2023, 6:58 PM +01:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 int count(long number);
 int luhn(long number);
 int main(void)
@@ -7,14 +8,16 @@ int main(void)
     while (1) {
         long input = get_long("Number: ");
         int length = count(input);
-        int initial = input / (10 ** (length - 2));
+        int initial = input / pow(10, length - 2);
         if (luhn(input) % 10 == 0) {
             if (length == 15 && (initial == 34 || initial == 37)) {
                 printf("AMEX\n");
-            } else if (length == 16 && (intial == 51 || initial == 52 || initial == 53 || initial == 54 || initial == 55)) {
+            } else if (length == 16 && (initial == 51 || initial == 52 || initial == 53 || initial == 54 || initial == 55)) {
                 printf("MASTERCARD\n");
             } else if ((length == 13 || length == 16) && (initial / 10 == 4)) {
-                printf("VISA\n")
+                printf("VISA\n");
+            } else {
+                printf("INVALID\n");
             }
         }
     }
