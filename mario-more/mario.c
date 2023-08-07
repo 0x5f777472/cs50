30ca4272cc56bc95d1f07c4a760eb0d44c65f894
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-07 16:25:14 +0000

    Mon, Aug 7, 2023, 5:25 PM +01:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,16 +3,26 @@
 
 int main(void)
 {
-    while (True) {
+    while (1) {
         int input = get_int("Height: ");
         if (input < 1 || input > 8) {
             break;
         }
         for (int i = 1; i <= input; i++) {
-            for (;;) {
-
+            for (int j = 0;j < input - i; j++) {
+                printf(" ");
             }
+            for (int j = 0; j < i; j++) {
+                printf("#");
+            }
+            printf("  ");
+            for (int j = 0; j < i; j++) {
+                printf("#");
+            }
+            printf("\n");
         }
+        break;
     }
+    return 0;
 
 }
