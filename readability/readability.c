d277e80aa9f3bd8ff4115abfe865d598e15a9056
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-09 08:05:08 +0000

    Wed, Aug 9, 2023, 9:05 AM +01:00

--- readability/readability.c
+++ readability/readability.c
@@ -10,10 +10,9 @@ int count_sentences(char *text);
 int main(void)
 {
     // uhh
-    // char input[2048];
-    // printf("Text: ");
-    // fgets(input, sizeof(input), stdin);
-    string input = get_string("Text: ");
+    char input[2048];
+    printf("Text: ");
+    fgets(input, sizeof(input), stdin);
     float W = count_words(input);
     float L = 100 * count_letters(input) / W;
     float S = 100 * count_sentences(input) / W;
