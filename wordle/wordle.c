647849cf4bfe25800dcc43b1a7ddd8b9350a6f85
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-09 16:16:43 +0000

    Wed, Aug 9, 2023, 5:16 PM +01:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -103,7 +103,14 @@ int main(int argc, string argv[])
 
     // Print the game's result
     // TODO #7
-
+    if (won)
+    {
+        printf("You won!\n");
+    }
+    else
+    {
+        printf("The word was %s\n", choice);
+    }
     // that's all folks!
     return 0;
 }
