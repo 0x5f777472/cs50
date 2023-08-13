ffc6b5971f88e35883d101ff6a0c509614a37225
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-13 16:18:39 +0000

    Sun, Aug 13, 2023, 5:18 PM +01:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -3,7 +3,17 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int a_mean = (image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3;
+            int g_mean = (image[i][j].rgbtRed * image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3;
+            image[i][j].rgbtRed = a_mean;
+            image[i][j].rgbtGreen = a_mean;
+            image[i][j].rgbtBlue = a_mean;
+        }
+    }
     return;
 }
 
