70c5b8da48a06cd9c0d769f2c84546a519e0144b
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-14 10:48:49 +0000

    Mon, Aug 14, 2023, 11:48 AM +01:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -118,6 +118,15 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                 Gx.rgbtBlue = -1 * (image[i - 1][j + 1] + 2 * image[i][j + 1].rgbtBlue + image[i + 1][j + 1].rgbtBlue);
             }
             else if (i == height - 1 && j = 0)
+            {
+                Gx.rgbtRed = 2 * image[i][j + 1].rgbtRed + image[i - 1][j + 1].rgbtRed;
+                Gx.rgbtGreen = 2 * image[i][j + 1].rgbtGreen + image[i - 1][j + 1].rgbtGreen;
+                Gx.rgbtBlue = 2 * image[i][j + 1].rgbtBlue + image[i - 1][j + 1].rgbtBlue;
+            }
+            else if (i == height - 1 && j > 0 && j < width - 1)
+            {
+                
+            }
         }
     }
     return;
