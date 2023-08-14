2a5c99cb99457f430dc4fb175f503ea9cc0f7d2c
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-14 10:59:57 +0000

    Mon, Aug 14, 2023, 11:59 AM +01:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -92,18 +92,27 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                 Gx.rgbtRed = 2 * image[i][j + 1].rgbtRed + image[i + 1][j + 1].rgbtRed;
                 Gx.rgbtGreen = 2 * image[i][j + 1].rgbtGreen + image[i + 1][j + 1].rgbtGreen;
                 Gx.rgbtBlue = 2 * image[i][j + 1].rgbtBlue + image[i + 1][j + 1].rgbtBlue;
+                Gy.rgbtRed = 2 * image[i + 1][j].rgbtRed + image[i + 1][j + 1].rgbtRed;
+                Gy.rgbtGreen = 2 * image[i + 1][j].rgbtGreen + image[i + 1][j + 1].rgbtGreen;
+                Gy.rgbtBlue = 2 * image[i + 1][j].rgbtBlue + image[i + 1][j + 1].rgbtBlue;
             }
             else if (i == 0 && j > 0 && j < width - 1)
             {
                 Gx.rgbtRed = 2 * image[i][j + 1].rgbtRed + image[i + 1][j + 1].rgbtRed - (2 * image[i][j - 1].rgbtRed + image[i + 1][j - 1].rgbtRed);
                 Gx.rgbtGreen = 2 * image[i][j + 1].rgbtGreen + image[i + 1][j + 1].rgbtGreen - (2 * image[i][j - 1].rgbtGreen + image[i + 1][j - 1].rgbtGreen);
                 Gx.rgbtBlue = 2 * image[i][j + 1].rgbtBlue + image[i + 1][j + 1].rgbtBlue - (2 * image[i][j - 1].rgbtBlue + image[i + 1][j - 1].rgbtBlue);
+                Gy.rgbtRed = image[i + 1][j - 1].rgbtRed + 2 * image[i + 1][j].rgbtRed + image[i + 1][j + 1].rgbtRed;
+                Gy.rgbtGreen = image[i + 1][j - 1].rgbtGreen + 2 * image[i + 1][j].rgbtGreen + image[i + 1][j + 1].rgbtGreen;
+                Gy.rgbtBlue = image[i + 1][j - 1].rgbtBlue + 2 * image[i + 1][j].rgbtBlue + image[i + 1][j + 1].rgbtBlue;
             }
             else if (i == 0 && j == width - 1)
             {
                 Gx.rgbtRed = -1 * (2 * image[i][j - 1].rgbtRed + image[i + 1][j - 1].rgbtRed);
                 Gx.rgbtGreen = -1 * (2 * image[i][j - 1].rgbtGreen + image[i + 1][j - 1].rgbtGreen);
                 Gx.rgbtBlue = -1 * (2 * image[i][j - 1].rgbtBlue + image[i + 1][j - 1].rgbtBlue);
+                Gy.rgbtRed = 2 * image[i + 1][j].rgbtRed + image[i + 1][j - 1].rgbtRed;
+                Gy.rgbtGreen = 2 * image[i + 1][j].rgbtGreen + image[i + 1][j - 1].rgbtGreen;
+                Gy.rgbtBlue = 2 * image[i + 1][j].rgbtBlue + image[i + 1][j - 1].rgbtBlue;
             }
             else if (i > 0 && i < height - 1 && j == 0)
             {
