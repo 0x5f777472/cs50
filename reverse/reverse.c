09a24996a0718a1ca57fcb015f2cb10b93729db7
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-24 09:22:07 +0000

    Thu, Aug 24, 2023, 10:22 AM +01:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -51,7 +51,7 @@ int main(int argc, char *argv[])
     }
     // Write header to file
     // TODO #6
-    fwrite();
+    fwrite(, sizeof,, output);
     // Use get_block_size to calculate size of block
     // TODO #7
 
