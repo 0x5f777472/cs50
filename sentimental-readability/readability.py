1f62b12e5167f7bd245322a01c9948cfecc9f24e
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-30 12:02:16 +0000

    Wed, Aug 30, 2023, 1:02 PM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,7 +1,10 @@
 # TODO
 def main():
     text = input("Text: ")
-    
+    W = count_words(text)
+    L = 100 * count_letters(text) / W
+    S = 100 * count_sentences(text) / W
+    index = round
 
 
 if __name__ == "__main__":
