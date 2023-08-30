2626d8142a11d74922b449d77488cdc5eb1294e0
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-30 15:55:31 +0000

    Wed, Aug 30, 2023, 4:55 PM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -11,8 +11,8 @@ def main():
     # TODO: Read database file into a variable
     with open(sys.argv[1], "r") as db:
         reader = csv.DictReader(db)
-    for row in reader:
-        print(row)
+        for row in reader:
+            print(row)
     # TODO: Read DNA sequence file into a variable
 
     # TODO: Find longest match of each STR in DNA sequence
