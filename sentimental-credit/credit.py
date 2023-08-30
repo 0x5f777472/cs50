ee38aadea472f27d9cb8b64e876528dce736d0e8
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-30 11:35:27 +0000

    Wed, Aug 30, 2023, 12:35 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -37,15 +37,12 @@ def nth(number, n):
 
 def luhn(number):
     sum = 0
-    for i in range(1, count(number)):
+    for i in range(1, count(number), 2):
         stupid = 2 * nth(number, i)
         for j in range(count(stupid)):
             sum += nth(stupid, j)
-            j += 1
-        i += 2
-    for i in range(count(number)):
+    for i in range(count(number), 2):
         sum += nth(number, i)
-        i += 2
     return sum
 
 
--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -6,16 +6,12 @@ while True:
             for i in range(1, height + 1):
                 for j in range(height - i):
                     print(" ", end="")
-                    j += 1
                 for j in range(i):
                     print("#", end="")
-                    j += 1
                 print("  ", end="")
                 for j in range(i):
                     print("#", end="")
-                    j += 1
                 print("")
-                i += 1
             break
     except:
         continue
