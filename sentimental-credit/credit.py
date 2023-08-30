55b75f9ddf18c48b9f97c0d20fe3d969527e0c7c
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-30 11:01:40 +0000

    Wed, Aug 30, 2023, 12:01 PM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -6,12 +6,23 @@ def main():
             length = count(number)
             initial = number // (10 ** (length - 2))
             if luhn(number) % 10 == 0:
-                if length == 15 &
+                if length == 15 and (intial == 34 or initial == 37):
+                    print("AMEX")
+                    break
+                elif length == 16 and (initial == 51 or initial == 52 or initial == 53 or initial == 54 or initial == 55):
+                    print("MASTERCARD")
+                    break
+                elif (initial // 10 == 4) and (length == 13 or length == 16):
+                    print("VISA")
+                    break
+                else:
+                    print("INVALID")
+                    break
         except:
             continue
 
 def nth(number, n):
-    return (number / (10 ** n)) % 10
+    return (number // (10 ** n)) % 10
 
 def luhn(number):
     sum = 0
