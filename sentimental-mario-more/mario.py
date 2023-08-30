292f68daa50cef7383f75396ad6bdfd3b264bd9c
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-30 10:44:26 +0000

    Wed, Aug 30, 2023, 11:44 AM +01:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,19 +1,21 @@
 # TODO
 while True:
-    height = int(input("Height: "))
-    if 1 <= height <= 8:
-        for i in range(1, height + 1):
-            for j in range(height - i):
-                print(" ", end="")
-                j+=1
-            for j in range(i):
-                print("#", end="")
-                j+=1
-            print("  ", end="")
-            for j in range(i):
-                print("#", end="")
-                j+=1
-            print("")
-            i+=1
-        break
-
+    try:
+        height = int(input("Height: "))
+        if 1 <= height <= 8:
+            for i in range(1, height + 1):
+                for j in range(height - i):
+                    print(" ", end="")
+                    j+=1
+                for j in range(i):
+                    print("#", end="")
+                    j+=1
+                print("  ", end="")
+                for j in range(i):
+                    print("#", end="")
+                    j+=1
+                print("")
+                i+=1
+            break
+    except:
+        continue
