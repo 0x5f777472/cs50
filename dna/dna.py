91b1af62ad0ab306459edcd5d8dff2944dfe8642
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-30 15:29:06 +0000

    Wed, Aug 30, 2023, 4:29 PM +01:00

--- dna/dna.py
+++ dna/dna.py
@@ -7,9 +7,9 @@ def main():
     # TODO: Check for command-line usage
     if len(sys.argv) != 3:
         print("Usage: python dna.py data.csv sequence.txt")
-        sys.exit(1)
+        return
     # TODO: Read database file into a variable
-
+    with open(argv[1], "r") as db
     # TODO: Read DNA sequence file into a variable
 
     # TODO: Find longest match of each STR in DNA sequence
