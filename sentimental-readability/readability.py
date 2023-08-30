6797cc7f0c706d6aa38a4ec4edb0b559c9f1e472
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-30 12:04:29 +0000

    Wed, Aug 30, 2023, 1:04 PM +01:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -4,8 +4,15 @@ def main():
     W = count_words(text)
     L = 100 * count_letters(text) / W
     S = 100 * count_sentences(text) / W
-    index = round
+    index = round(0.0588 * L - 0.296 * S - 15.8)
+    if index >= 16:
+        print("Grade 16+")
+    elif index < 1:
+        print("Before Grade 1")
+    else
+        print(f"Grade {index}")
 
-
+def count_letters(text):
+    
 if __name__ == "__main__":
     main()
