181bce44a30dd4e563ff119d81c1d8eb83819c27
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-30 10:57:52 +0000

    Wed, Aug 30, 2023, 11:57 AM +01:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,8 +3,31 @@ def main():
     while True:
         try:
             number = int(input("Number: "))
-            
+            length = count(number)
+            initial = number // (10 ** (length - 2))
+            if luhn(number) % 10 == 0:
+                if length == 15 &
         except:
             continue
+
+def nth(number, n):
+    return (number / (10 ** n)) % 10
+
+def luhn(number):
+    sum = 0
+    for i in range(1, count(number)):
+        stupid = 2 * nth(number, i)
+        for j in range(count(stupid)):
+            sum += nth(stupid, j)
+            j += 1
+        i += 2
+    for i in range(count(number)):
+        sum += nth(number, i)
+        i += 2
+    return sum
+
+def count(number):
+    return len(str(number))
+
 if __name__ == '__main__':
     main()
