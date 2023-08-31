a419463a657498345c8947b2e63dde009c63608e
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-31 10:07:15 +0000

    Thu, Aug 31, 2023, 11:07 AM +01:00

---
+++ movies/12.sql
@@ -0,0 +1,2 @@
+SELECT title FROM movies JOIN stars on movies.id = stars.movie_id JOIN people ON stars.person_id = people.id WHERE name = "Bradley Cooper" AND name = "Jennifer Lawrence";
+SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Bradley Cooper" OR "Jennifer Lawrence"));
