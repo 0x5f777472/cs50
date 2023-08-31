68673e198cd62157ea5846a9868ab2bb75512bf6
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-31 14:54:58 +0000

    Thu, Aug 31, 2023, 3:54 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
--- crime scene descriptions:
+-- crime scene descriptions say stolen at 7/28 10:15 am at bakery
 SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
