72be1334b177099ac64149c4efa937d2ecfe7f16
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-31 16:18:06 +0000

    Thu, Aug 31, 2023, 5:18 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -10,8 +10,12 @@ SELECT hour, minute, activity, license_plate FROM bakery_security_logs WHERE mon
 --get flights the car owner ahve been on
 SELECT flight_id FROM passengers WHERE passport_number IN (SELECT passport_number FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND activity = 'exit'));
 SELECT id FROM flights WHERE origin_airport_id = 8 AND month = 7 AND day = 29;
--- flight 36.
+-- flight 36 is earliest flight
 -- get people (Stack overflow so split up (sorry if reading this))
 SELECT phone_number FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE license_plate IN (SELECT license_plate FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE caller IN (SELECT phone_number FROM people WHERE passport_number IN (SELECT passport_number FROM passengers WHERE passport_number IN (SELECT passport_number FROM people WHERE id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE account_number IN (SELECT account_number FROM bank_accounts WHERE person_id IN (SELECT id FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE month = 7 AND day = 28 AND duration < 60))) AND month = 7 AND day = 28 AND atm_location = 'Leggett Street'))) AND flight_id = 36)) AND day=28 AND duration < 60)) AND day=28 AND hour=10 AND minute<25 AND minute>15);
--- this returns (367) 555-5533
-SELECT name FROM people WHERE phone_number IN (SELECT caller, receiver FROM phone_calls WHERE caller = '(367) 555-5533' AND day=28 AND duration<60);
+-- ^this returns (367) 555-5533
+-- this (below) gives names of the people
+SELECT name FROM people WHERE phone_number IN (SELECT caller FROM phone_calls WHERE caller = '(367) 555-5533' AND day=28 AND duration<60);
+SELECT name FROM people WHERE phone_number IN (SELECT receiver FROM phone_calls WHERE caller = '(367) 555-5533' AND day=28 AND duration<60);
+-- this below gives destination of his flight
+SELECT city FROM airports WHERE id IN (SELECT destination_airport_id FROM flights WHERE id IN (SELECT flight_id FROM passengers WHERE passport_number IN (SELECT passport_number FROM people WHERE phone_number = '(367) 555-5533')));
