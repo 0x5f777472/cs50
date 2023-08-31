80d5b2c02066584daf774c54558ee11ebbe6da46
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-31 09:59:32 +0000

    Thu, Aug 31, 2023, 10:59 AM +01:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,1 @@
-SELECT title, rating FROM movies, ratings WHERE [year] = "2010" AND ratings.movie_id = movies.id ORDER BY ratings;
+SELECT title, rating FROM movies, ratings WHERE [year] = "2010" AND ratings.movie_id = movies.id ORDER BY rating DESC;
