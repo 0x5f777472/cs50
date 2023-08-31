5b6551c08a68705eccef0f3d2769a2d4623092da
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-31 15:35:47 +0000

    Thu, Aug 31, 2023, 4:35 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,7 +3,7 @@
 -- crime scene descriptions say stolen at 7/28 10:15 am at bakery
 SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
 -- bakery logs:
-SELECT hour, minute, activity, license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND (hour = 10 OR hour = 9);
+SELECT hour, minute, activity, license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND activity = 'exit';
 
 SELECT * FROM interviews WHERE month = 7 AND day = 28;
 -- before 10:15 am, leggett street, withdrawing. 10 minutes within the theft drive away. call for less than 1 minute, earliest flight tomorrow, accomplice buys ticket.
