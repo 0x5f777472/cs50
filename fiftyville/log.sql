3fdb83028a52b07d26410dce7fd1943ee2e835f0
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-08-31 15:39:08 +0000

    Thu, Aug 31, 2023, 4:39 PM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -2,9 +2,10 @@
 
 -- crime scene descriptions say stolen at 7/28 10:15 am at bakery
 SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = 'Humphrey Street';
+-- before 10:15 am, leggett street, withdrawing. 10 minutes within the theft drive away. call for less than 1 minute, earliest flight tomorrow, accomplice buys ticket.
+SELECT * FROM interviews WHERE month = 7 AND day = 28;
+
 -- bakery logs:
 SELECT hour, minute, activity, license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND activity = 'exit';
-
-SELECT * FROM interviews WHERE month = 7 AND day = 28;
--- before 10:15 am, leggett street, withdrawing. 10 minutes within the theft drive away. call for less than 1 minute, earliest flight tomorrow, accomplice buys ticket.
-
+--get flights the car owner ahve been on
+SELECT flight_id FROM passengers WHERE passport_number IN (SELECT passport_number FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND activity = 'exit'));
