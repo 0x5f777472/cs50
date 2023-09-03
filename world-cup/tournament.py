1ed4e9123fc91571b15eff842dec9f6c71675f3f
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-03 13:27:52 +0000

    Sun, Sep 3, 2023, 2:27 PM +01:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,8 +16,9 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
-    with open(sys.argv[1]) as csv:
-        
+    with open(sys.argv[1], "r") as db:
+        teams = list(csv.DictReader(db))
+    print(simulate_round(teams))
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
 
