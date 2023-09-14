aee472c30b9a32143fd98a1b6e1a5d487ec5fc6e
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-14 20:25:02 +0000

    Thu, Sep 14, 2023, 9:25 PM +01:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -103,7 +103,7 @@ bool unload(void)
             free(cur);
             n = n->next;
         }
-        if ()
+        if (n == NULL && i == N - 1)
         {
             return true;
         }
