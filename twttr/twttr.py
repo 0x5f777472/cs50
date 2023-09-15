717b400398f03b89153a13d401e11a5e11d31b77
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-15 20:28:36 +0000

    Fri, Sep 15, 2023, 9:28 PM +01:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,3 +1,6 @@
 tweet = input("Input: ")
+print("Output: ", end = "")
 for i in range(len(tweet)):
-    
+    if tweet[i].lower() not in ["a", "e", "i", "o", "u"]:
+        print(tweet[i], end = "")
+print()
