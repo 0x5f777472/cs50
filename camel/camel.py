6bad2213c0e0cd97c5a276cde8e78ebc9d86060c
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-15 20:08:51 +0000

    Fri, Sep 15, 2023, 9:08 PM +01:00

--- camel/camel.py
+++ camel/camel.py
@@ -4,6 +4,6 @@ last = 0
 for i, c in enumerate(camel):
     if c.isupper():
         camel_split.append(camel[last:i])
-        print(camel[last:i])
+        print(f"last: {last}, i: {i}, c: {c}")
         last = i
 print(camel_split)
