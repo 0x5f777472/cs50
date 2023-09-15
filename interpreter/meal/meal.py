5d8966b6090207e80301035f397b72b5f6858d7c
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-15 19:44:45 +0000

    Fri, Sep 15, 2023, 8:44 PM +01:00

--- interpreter/meal/meal.py
+++ interpreter/meal/meal.py
@@ -1,12 +1,18 @@
 def main():
-
+    time_value = convert(input("What time is it? "))
+    if 7 <= time_value <= 8:
+        print("breakfast time")
+    elif 12 <= time_value <= 13:
+        print("lunch time")
+    elif 18 <= time_value <= 19:
+        print("dinner time")
 
 
 def convert(time):
     time_list = time.split(":")
     hour = int(time_list[0])
-    minutes = int(time_list[1])
-    return hour +
+    minutes = float(time_list[1])
+    return hour + minutes / 60
 
 
 
