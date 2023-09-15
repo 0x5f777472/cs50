76787d95dc9ff6d8f490b66edcb2e98cf2f71023
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-15 20:49:00 +0000

    Fri, Sep 15, 2023, 9:49 PM +01:00

--- plates/plates.py
+++ plates/plates.py
@@ -10,7 +10,17 @@ def is_valid(s):
     if 2 <= len(s) <= 6:
         if s.isalpha():
             return True
-        if s[:2].isalpha()
+        if s[:2].isalpha():
+            for i in range(2, len(s)):
+                if s[-(len(s) - i):].isnumeric():
+                    passed = 1
+            safe = 1
+            for i in range(len(s)):
+                if s[i].isnumeric() and s[i] == 0:
+                    safe = 0
+                    break
+            if passed and safe:
+                return True
     return False
 
 main()
