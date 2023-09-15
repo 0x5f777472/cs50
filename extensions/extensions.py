4c7949343a2b9bb2cd0b5821086ed6bc01979f80
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-15 19:17:44 +0000

    Fri, Sep 15, 2023, 8:17 PM +01:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,2 +1,17 @@
 name = input("File name: ")
-index = name[name.rfind("."):]
+ext = name[name.rfind("."):]
+match ext:
+    case ".gif":
+        print("image/gif")
+    case ".jpg" | ".jpeg":
+        print("image/jpeg")
+    case ".png":
+        print("image/png")
+    case ".pdf":
+        print("application/pdf")
+    case ".txt":
+        print("text/plain")
+    case ".zip":
+        print("application/zip")
+    case _:
+        print("application/octet-stream")
