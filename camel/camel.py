799e967fac9ce03318870d101b1a4a548e939173
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-15 20:06:46 +0000

    Fri, Sep 15, 2023, 9:06 PM +01:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,9 +1,8 @@
 camel = input("camelCase: ")
 camel_split = []
+last = 0
 for i, c in enumerate(camel):
     if c.isupper():
-        last = 0
         camel_split.append(camel[last:i])
-        print(camel[last:i])
         last = i
 print(camel_split)
