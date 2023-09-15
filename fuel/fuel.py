0fae483f7d4c881e92e503ae275de1e3b0f69af2
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-15 21:56:48 +0000

    Fri, Sep 15, 2023, 10:56 PM +01:00

---
+++ fuel/fuel.py
@@ -0,0 +1,18 @@
+while True:
+    gauge = input("Fraction: ")
+    try:
+        x, y = gauge.split("/")
+        x = int(x)
+        y = int(y)
+        if x > y or y == 0:
+            pass
+        frac = 100 * x/y
+        if frac > 99:
+            print("F")
+        elif frac < 1:
+            print("E")
+        else:
+            print(f"{round(frac)}%")
+
+    except (ValueError, ZeroDivisionError):
+        pass
