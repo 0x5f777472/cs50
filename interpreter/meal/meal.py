6f5a5de2d8ce09a630dbeef58dde6ecfd35cfc7a
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-15 19:41:29 +0000

    Fri, Sep 15, 2023, 8:41 PM +01:00

---
+++ interpreter/meal/meal.py
@@ -0,0 +1,14 @@
+def main():
+
+
+
+def convert(time):
+    time_list = time.split(":")
+    hour = int(time_list[0])
+    minutes = int(time_list[1])
+    return hour +
+
+
+
+if __name__ == "__main__":
+    main()
