45c1cd070f5f870406225324ffdf0cf84f94b49b
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 22:30:09 +0000

    Sat, Sep 16, 2023, 11:30 PM +01:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -5,11 +5,11 @@ if len(sys.argv) < 3:
 elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
 elif sys.argv[1][-4:] != ".csv":
-    sys.exit(f"Could not read {sys.argv[1]}")
+    sys.exit("Invalid input file name")
 else:
     try:
         with open(sys.argv[1], "r") as file:
             lines = list(csv.reader(file))
-        print(tabulate(lines, headers="firstrow", tablefmt="grid"))
+        print(lines)
     except EnvironmentError:
-        sys.exit("File does not exist")
+        sys.exit(f"Could not read {sys.argv[1]}")
