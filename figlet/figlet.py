97f3f61c37186f85b0ab0a53bca9b2419a11c9ac
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 08:46:34 +0000

    Sat, Sep 16, 2023, 9:46 AM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,4 +1,7 @@
 import sys
 from pyfiglet import Figlet
 word = input("Input: ")
+if len(sys.argv) == 1:
+    print("Output:\n" + Figlet(font=random).renderText(word))
+
 print("Output:\n" + Figlet(font=sys.argv[2]).renderText(word))
