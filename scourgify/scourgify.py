84b8d665b1f2f00d5971bd3982b82f2d33000662
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 22:30:50 +0000

    Sat, Sep 16, 2023, 11:30 PM +01:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -5,7 +5,7 @@ if len(sys.argv) < 3:
 elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
 elif sys.argv[1][-4:] != ".csv":
-    sys.exit("Invalid input file name")
+    sys.exit("Not a CSV file")
 else:
     try:
         with open(sys.argv[1], "r") as file:
