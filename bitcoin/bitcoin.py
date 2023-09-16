402e977232df85e9d1ce37229bdcae03a5d46a00
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 14:48:34 +0000

    Sat, Sep 16, 2023, 3:48 PM +01:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,1 +1,17 @@
 import requests
+import sys
+
+try:
+    if len(sys.argv) == 2:
+        multiplier = float(sys.argv[1])
+        price = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json').json()["bpi"]["USD"]["rate_float"]
+        print(f"{(price * multiplier):,.4f}")
+    elif len(sys.argv) == 1:
+        print("Missing command-line argument")
+        sys.exit(1)
+except requests.RequestException:
+    print("could not fetch a the data :-(")
+    sys.exit(2)
+except ValueError:
+    print("Command-line argument is not a number")
+    sys.exit(3)
