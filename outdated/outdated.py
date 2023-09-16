71b0edf389a1a17b44eafbb8b15dd88b766dc78c
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 08:09:10 +0000

    Sat, Sep 16, 2023, 9:09 AM +01:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -12,12 +12,23 @@ months = [
     "November",
     "December"
 ]
-date = input("Date: ")
-try:
-    month, day, year = date.split("/")
-    print(month,day,year)
-except:
-    month, day, year = date.split(" ")
-    day = day.rstrip(",")
-    month = months.index[month] + 1
-    print(month,day,year)
+while True:
+    date = input("Date: ")iiiiiiiiio
+    try:
+        if date.split("/")[0].isnumeric():
+            month, day, year = date.split("/")
+            month, day, year = int(month), int(day), int(year)
+            if month - 1 in range(12) and day - 1 in range(31):
+                print(f"{year:02}-{month:02}-{day:02}")
+                break
+
+        else:
+            month, day, year = date.split(" ")
+            day = day.rstrip(",")
+            month = months.index(month) + 1
+            month, day, year = int(month), int(day), int(year)
+            if month - 1 in range(12) and day - 1 in range(31):
+                print(f"{year:02}-{month:02}-{day:02}")
+                break
+    except:
+        continue
