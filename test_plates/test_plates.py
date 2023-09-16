193697770ef094177a82ef2d05d62877012244f9
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 19:02:31 +0000

    Sat, Sep 16, 2023, 8:02 PM +01:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -3,18 +3,20 @@ from plates import is_valid
 def test_alphabetical():
     assert is_valid("ACBD") == True
     assert is_valid("AAAAAA") == True
-    assert is_valid("pp") == True
+    assert is_valid("PP") == True
 
 def test_middle_numbers():
     assert is_valid("AB21CD") == False
     assert is_valid("PP2C") == False
 
 def test_length():
-    assert is_valid("LahDeeDah") == False
-    assert is_valid("LahDee") == True
+    assert is_valid("LAHDEEDAH") == False
+    assert is_valid("LAHDEE") == True
     assert is_valid("Q") == False
 
 def test_end_numbers():
     assert is_valid("RU2112") == True
     assert is_valid("RUSH2112") == False
-    assert is_valid("boobs1") == True
+    assert is_valid("BOOB1") == True
+    assert is_valid("BO0BS1") == False
+    assert is_valid("BB01") == False
