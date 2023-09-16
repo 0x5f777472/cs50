70e21481112398914f9dcb163e00a8fa5f982e47
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 17:11:03 +0000

    Sat, Sep 16, 2023, 6:11 PM +01:00

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -1,10 +1,11 @@
 def main():
     tweet = input("Input: ")
+    print("Output: ", end="")
     print(shorten(tweet))
 
 
 def shorten(word):
     no_vowels = str.maketrans("","", "AEIOUaeiou")
-    return f"Output: {word.translate(no_vowels)}"
+    return word.translate(no_vowels)
 if __name__ == "__main__":
     main()
