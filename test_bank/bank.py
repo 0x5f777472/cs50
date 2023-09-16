26f9511b1472fde357062ed48433012969b6bfeb
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 18:52:04 +0000

    Sat, Sep 16, 2023, 7:52 PM +01:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -1,15 +1,15 @@
 def main():
     greeting = input("Greeting: ")
-    print(value(greeting))
+    print(f"${value(greeting)}")
 
 
 def value(greeting):
     if greeting.strip()[:5].lower() == "hello":
-        return "$0"
+        return 0
     elif greeting[0].lower() == "h":
-        return "$20"
+        return 20
     else:
-        return "$100"
+        return 100
 
 
 if __name__ == "__main__":
--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,7 +1,7 @@
 from bank import value
 
 def test_value():
-    assert value("Hello! You are stupid.") == "$0"
-    assert value("Heyy! wassup!") == "$20"
-    assert value("Eyy! wassup!") == "$100"
-    assert value("1001001") == "$100"
+    assert value("Hello! You are stupid.") == 0
+    assert value("Heyy! wassup!") == 20
+    assert value("Eyy! wassup!") == 100
+    assert value("1001001") == 100
