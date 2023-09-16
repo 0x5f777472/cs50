c91c93e0df2b64a1c78521ec50e4c1f3f8ae4763
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 22:00:08 +0000

    Sat, Sep 16, 2023, 11:00 PM +01:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,2 +1,24 @@
 import csv
 import tabulate
+import sys
+if len(sys.argv) < 2:
+    print("Too few command-line arguments")
+    sys.exit(1)
+elif len(sys.argv) > 2:
+    print("Too many command-line arguments")
+    sys.exit(1)
+elif sys.argv[1][-3:] != ".csv":
+    print("Not a CSV file")
+    sys.exit(1)
+else:
+    try:
+        with open(sys.argv[1], "r") as file:
+            lines = file.readlines()
+        line_count = 0
+        for line in lines:
+            if not line.isspace():
+                if line.lstrip()[0] != "#":
+                    line_count += 1
+        print(line_count)
+    except EnvironmentError:
+        print("oops")
