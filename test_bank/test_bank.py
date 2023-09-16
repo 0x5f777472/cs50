e94e422594cb6a36ad1afb49d62e880afa349933
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 18:49:34 +0000

    Sat, Sep 16, 2023, 7:49 PM +01:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -2,5 +2,6 @@ from bank import value
 
 def test_value():
     assert value("Hello! You are stupid.") == "$0"
-    assert value("heyy! wassup!") == "$20"
-    assert value("eyy! wassup!") == "$100"
+    assert value("Heyy! wassup!") == "$20"
+    assert value("Eyy! wassup!") == "$100"
+    assert value("1001001") == "$100"
