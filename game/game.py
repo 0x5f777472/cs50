ee190ed0f4ef12025144919132dc6ddcd8a1c189
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 13:44:20 +0000

    Sat, Sep 16, 2023, 2:44 PM +01:00

--- game/game.py
+++ game/game.py
@@ -1,8 +1,14 @@
-import random
+from random import randint
 while True:
     try:
         level = int(input("Level: "))
         if level < 1:
             continue
+        break
+    except:
+        continue
+while True:
+    try:
+        guess = int(input("Guess: "))
     except:
         continue
