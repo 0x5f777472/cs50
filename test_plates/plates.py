580ae736d41291e34915bb96623040cd416e614c
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 18:59:34 +0000

    Sat, Sep 16, 2023, 7:59 PM +01:00

--- test_plates/plates.py
+++ test_plates/plates.py
@@ -1,9 +1,20 @@
 def main():
-    ...
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
 
 
 def is_valid(s):
-    ...
+    if 2 <= len(s) <= 6 and s.isalnum():
+        if s.isalpha():
+            return True
+        if s[:2].isalpha() and s[-2:].isnumeric():
+            for i in range(len(s)):
+                if s[i].isdigit():
+                    return not (s[i] == "0" or s[i:].isalpha())
+    return False
 
 
 if __name__ == "__main__":
---
+++ test_plates/test_plates.py
@@ -0,0 +1,20 @@
+from plates import is_valid
+
+def test_alphabetical():
+    assert is_valid("ACBD") == True
+    assert is_valid("AAAAAA") == True
+    assert is_valid("pp") == True
+
+def test_middle_numbers():
+    assert is_valid("AB21CD") == False
+    assert is_valid("PP2C") == False
+
+def test_length():
+    assert is_valid("LahDeeDah") == False
+    assert is_valid("LahDee") == True
+    assert is_valid("Q") == False
+
+def test_end_numbers():
+    assert is_valid("RU2112") == True
+    assert is_valid("RUSH2112") == True
+    assert is_valid("boobs1") == True
