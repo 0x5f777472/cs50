d0ceb946bfb8740d3fdccb2f735c5e3f79e02bc5
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 19:14:35 +0000

    Sat, Sep 16, 2023, 8:14 PM +01:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -1,9 +1,11 @@
 from fuel import convert, gauge
-import pytest
+from pytest import raises
 
 def test_convert():
     assert convert("1/2") == 50
     assert convert("99/100") == 99
+    with pytest.raises(ZeroDivisionError, match='must be 0 or None'):
+        raise ZeroDivisionError('value must be 0 or None')
 
 def test_gauge():
     ...
