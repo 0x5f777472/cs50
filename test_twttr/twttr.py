d29a96920ea11cb802e5097f3c3c5c7e129ae2b3
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 17:05:15 +0000

    Sat, Sep 16, 2023, 6:05 PM +01:00

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -4,7 +4,7 @@ def main():
 
 
 def shorten(word):
-    mydict= {65:None,69:None,73:None,79:None,85:None,97:None,101:None,105:None,111:None,117:None}
-    return f"Output: {word.translate(mydict)}"
+    no_vowels = str.maketrans("aeiouAEIOU", None)
+    return f"Output: {word.translate(no_vowels)}"
 if __name__ == "__main__":
     main()
