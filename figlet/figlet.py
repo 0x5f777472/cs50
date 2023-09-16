8eb2c015233749e5a357b2d0830208bc28c55c31
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 13:11:09 +0000

    Sat, Sep 16, 2023, 2:11 PM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,11 +2,12 @@ import sys
 import random
 from pyfiglet import Figlet
 if len(sys.argv) == 1:
-word = input("Input: ")
+    word = input("Input: ")
     f = Figlet(font=random.choice(Figlet().getFonts()))
     print("Output:")
     print(f.renderText(word))
 elif len(sys.argv) == 3 and sys.argv[1] in ["-f", "--font"] and sys.argv[2] in Figlet().getFonts():
+    word = input("Input: ")
     f = Figlet(font=sys.argv[2])
     print("Output:")
     print(f.renderText(word))
