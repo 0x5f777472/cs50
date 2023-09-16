c83b83548c71246c902d3a4e3f4e82652230eece
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 13:52:15 +0000

    Sat, Sep 16, 2023, 2:52 PM +01:00

---
+++ professor/professor.py
@@ -0,0 +1,17 @@
+import random
+def main():
+    ...
+
+
+def get_level():
+    while True:
+        try:
+            level = int(input("Level: "))
+
+
+def generate_integer(level):
+    ...
+
+
+if __name__ == "__main__":
+    main()
