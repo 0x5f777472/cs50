e65073f9a69a3c6ca109683bd9841a6771a508e9
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 20:05:06 +0000

    Sat, Sep 16, 2023, 9:05 PM +01:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,7 +1,13 @@
 import sys
-if sys.argv == 1:
+if len(sys.argv) < 2:
     print("Too few command-line arguments")
     sys.exit(1)
-elif sys.argv > 2:
+elif len(sys.argv) > 2:
     print("Too many command-line arguments")
     sys.exit(1)
+elif sys.argv[1][-3:] != ".py":
+    print("Not a Python file")
+    sys.exit(1)
+else:
+    with open(sys.argv[1], "r") as file:
+        ...
