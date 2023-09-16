3ef4aec31a686a85f84f9e6c76fc1ebbf58879a5
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 08:56:22 +0000

    Sat, Sep 16, 2023, 9:56 AM +01:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -4,7 +4,6 @@ from pyfiglet import Figlet
 figlet = Figlet()
 word = input("Input: ")
 if len(sys.argv) == 1:
-    font = random.choice(figlet.getFonts())
-    print("Output:\n" + Figlet(font=random).renderText(word))
-
-print("Output:\n" + Figlet(font=sys.argv[2]).renderText(word))
+    font=random.choice(figlet.getFonts())
+    print("Output:\n" + figlet.figlet_format(word))
+elif len(sys.argv) == 3 and sys.argv[1] in ["-f", "--font"]:
