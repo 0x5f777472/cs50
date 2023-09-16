bd1b94328f563a8057e49727ecf9472eabfad73a
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 16:25:16 +0000

    Sat, Sep 16, 2023, 5:25 PM +01:00

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -1,18 +1,14 @@
 def main():
-    ...
+    tweet = input("Input: ")
+    print("Output: ", end = "")
+    shorten(tweet)
 
 
 def shorten(word):
-    ...
-
+    for i in range(len(word)):
+        if word[i].lower() not in ["a", "e", "i", "o", "u"]:
+            print(word[i], end = "")
+    print()
 
 if __name__ == "__main__":
     main()
-
-
-tweet = input("Input: ")
-print("Output: ", end = "")
-for i in range(len(tweet)):
-    if tweet[i].lower() not in ["a", "e", "i", "o", "u"]:
-        print(tweet[i], end = "")
-print()
