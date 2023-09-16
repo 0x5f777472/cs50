985e7a6c5dea49646bcebeb9de0098a1ed5236b8
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 17:07:05 +0000

    Sat, Sep 16, 2023, 6:07 PM +01:00

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -4,7 +4,7 @@ def main():
 
 
 def shorten(word):
-    no_vowels = str.maketrans("", "", "AEIOUaeiou")
+    no_vowels = str.maketrans("","", "AEIOUaeiou")
     return f"Output: {word.translate(no_vowels)}"
 if __name__ == "__main__":
     main()
