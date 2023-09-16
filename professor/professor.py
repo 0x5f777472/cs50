35b106a58d58a572e2c53e99da142f4f1f313b7b
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 14:15:08 +0000

    Sat, Sep 16, 2023, 3:15 PM +01:00

--- professor/professor.py
+++ professor/professor.py
@@ -17,10 +17,15 @@ def get_level():
 def generate_integer(level):
     match level:
         case 1:
-            x = 
+            x = randint(0, 9)
+            y = randint(0, 9)
         case 2:
+            x = randint(10, 99)
+            y = randint(10, 99)
         case 3:
-
+            x = randint(100, 999)
+            y = randint(100, 999)
+        return x, y
 
 
 if __name__ == "__main__":
