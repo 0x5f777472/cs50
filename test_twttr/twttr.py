781f46ac86f62ba1ad6bca04e4e6e46ecd29e009
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-16 17:02:14 +0000

    Sat, Sep 16, 2023, 6:02 PM +01:00

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -4,11 +4,8 @@ def main():
 
 
 def shorten(word):
-    mydict: {}
-    for i in range(len(word)):
-        if word[i].lower() in ["a", "e", "i", "o", "u"]:
-            word.delete(word[i])
-    return f"Output: {word}"
+    mydict= {97:None,101:None,105:None,111:None,117:None}
+    return f"Output: {word.translate(mydict)}"
 
 if __name__ == "__main__":
     main()
