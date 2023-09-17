b94d5a72907d0d9bc27fae35ee4768dcc1602119
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-17 15:04:18 +0000

    Sun, Sep 17, 2023, 4:04 PM +01:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -4,8 +4,8 @@ if len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
-elif sys.argv[1].endswith():
-    sys.exit("Not a CSV file")
+elif sys.argv[1].endswith((".jpg", ".jpeg", ".png")):
+    sys.exit("Invalid input")
 else:
     try:
         ...
