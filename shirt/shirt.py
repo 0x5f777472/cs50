648b64ebf7a989e2397e3fedefe95440f3e4a565
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-17 15:16:28 +0000

    Sun, Sep 17, 2023, 4:16 PM +01:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -4,10 +4,10 @@ if len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
-elif sys.argv[1].rpartition('.')[2].lower() not in [".jpg", ".jpeg", ".png"] or sys.argv[2].rpartition('.')[2].lower() not in [".jpg", ".jpeg", ".png"]:
+elif sys.argv[1].rpartition('.')[2].lower() not in ["jpg", "jpeg", "png"] or sys.argv[2].rpartition('.')[2].lower() not in ["jpg", "jpeg", "png"]:
     sys.exit("Invalid input")
-elif sys.argv[1][-4:].lower() != sys.argv[2][-4:].lower():
-    ...
+elif sys.argv[1].rpartition('.')[2].lower() != sys.argv[2].rpartition('.')[2].lower():
+    sys.exit("Input and output have different extensions")
 else:
     try:
         ...
