c418d67be43cfeef8ab83b9b5ab4d7ce14d0a18e
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-17 15:09:30 +0000

    Sun, Sep 17, 2023, 4:09 PM +01:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -4,8 +4,10 @@ if len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
-elif sys.argv[1].endswith((".jpg", ".jpeg", ".png")):
+elif not sys.argv[1].endswith((".jpg", ".jpeg", ".png")) or not sys.argv[2].endswith((".jpg", ".jpeg", ".png")):
     sys.exit("Invalid input")
+elif sys.argv[1][-4:].lower() != sys.argv[2][-4:].lower():
+    ...
 else:
     try:
         ...
