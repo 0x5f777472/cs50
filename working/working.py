e7954651ebb70a6b31d595ca853718a39f7378c5
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-18 23:00:08 +0000

    Tue, Sep 19, 2023, 12:00 AM +01:00

--- working/working.py
+++ working/working.py
@@ -6,7 +6,8 @@ def main():
 
 
 def convert(s):
-    m = re.search(r"()", s)
+    m = re.search(r"([1-9]|1[0-2])(?::([0-5][0-9])) (AM|PM) to ([1-9]|1[0-2])(?::([0-5][0-9])) (AM|PM)?", s)
+    return m.groups()
 
 
 if __name__ == "__main__":
