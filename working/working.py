8ce2f45d9d48c5360ac66c840db6692d2dd4124a
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-18 23:06:13 +0000

    Tue, Sep 19, 2023, 12:06 AM +01:00

--- working/working.py
+++ working/working.py
@@ -7,8 +7,15 @@ def main():
 
 def convert(s):
     if m := re.search(r"([1-9]|1[0-2])(?::([0-5][0-9]))? (AM|PM) to ([1-9]|1[0-2])(?::([0-5][0-9]))? (AM|PM)", s):
-        return m.groups()
-
+        h1 = m.group(1)
+        p1 = m.group(3)
+        h2 = m.group(4)
+        p2 = m.group(6)
+        if None in m.groups:
+            return f"{h1:02}:00 to {h2:02}:00"
+        m1 = m.group(2)
+        m2 = m.group(5)
+        return f"{h1:02}:{m1} to {h2:02}:{m2}"
 
 if __name__ == "__main__":
     main()
