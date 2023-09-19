7ba27c7efe236a244aa056c8df673ab5614bb73d
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-19 00:01:10 +0000

    Tue, Sep 19, 2023, 1:01 AM +01:00

--- working/test_working.py
+++ working/test_working.py
@@ -1,4 +1,14 @@
 from working import convert
+from pytest import raises
 
 def test_not_times():
-    assert convert("cat") 
+    with raises(ValueError):
+        convert("cat")
+    with raises(ValueError):
+        convert("don't blink")
+
+def no_minutes():
+    assert convert("9 AM to 5 PM") == "9:00 to 17:00"
+
+def yes_minutes():
+    assert convert("9:00 AM to 5:00 AM") == "9:00 to 17:00"
