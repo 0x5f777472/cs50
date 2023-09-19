5225707ecbfa32155c4ce94a705c8f4fed5ceb61
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-19 08:51:13 +0000

    Tue, Sep 19, 2023, 9:51 AM +01:00

--- um/um.py
+++ um/um.py
@@ -6,7 +6,7 @@ def main():
 
 
 def count(s):
-    m = re.findall(r"(?:[^A-Za-z0-9]|\b)um(?:[^A-Za-z0-9]|\b)", s.lower())
+    m = re.findall(r"(?:\W|\b)um(?:\W|\b)", s.lower())
     return len(m)
 
 
