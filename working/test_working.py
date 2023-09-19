70d356b8b46afdbf8c57644ab773c97dd81886ad
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-19 00:05:12 +0000

    Tue, Sep 19, 2023, 1:05 AM +01:00

--- working/test_working.py
+++ working/test_working.py
@@ -7,10 +7,10 @@ def test_not_times():
     with raises(ValueError):
         convert("don't blink")
     with raises(ValueError):
-        convert("12:60 PM to 13:120 AM")
+        convert("12:60 PM to 13:12 AM")
 
 def test_no_minutes():
-    assert convert("9 AM to 5 PM") == "9:00 to 17:00"
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
 
 def test_yes_minutes():
-    assert convert("9:00 AM to 5:00 AM") == "9:00 to 17:00"
+    assert convert("9:00 AM to 5:00 AM") == "09:00 to 05:00"
