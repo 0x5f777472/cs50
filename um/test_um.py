f3496b85bb08620f7519464ccb27c71f2fcac96e
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-19 08:55:09 +0000

    Tue, Sep 19, 2023, 9:55 AM +01:00

--- um/test_um.py
+++ um/test_um.py
@@ -1,1 +1,13 @@
 from um import count
+
+def test_individual():
+    assert count("blah um blah um blah") == 2
+    assert count("blah um blah") == 1
+
+def test_ends():
+    assert count("Um blah Um") == 2
+    assert count("Um blah") == 2
+
+def test_punctuation():
+    assert count("um, what? what, um, are, um, you, um, saying???? stop saying um") == 5
+    assert count("um, do i have to swallow your cum?") == 1
