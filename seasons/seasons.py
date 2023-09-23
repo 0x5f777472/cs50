c829522ba5bb0ec73726b2bad6f9b02cd775b189
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-23 15:14:16 +0000

    Sat, Sep 23, 2023, 4:14 PM +01:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -2,11 +2,15 @@ from datetime import date
 
 
 def main():
-    ...
+    birth = get_birth()
+    timedelta = get_timedelta(birth)
+    mins = get_mins(timedelta)
+    print(wordify(mins))
 
 
-...
-
+def get_birth():
+    birthdate = input("Date of Birth: ")
+    
 
 if __name__ == "__main__":
     main()
