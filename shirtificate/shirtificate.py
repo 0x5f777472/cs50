4cb909a18a2cbd8288fbb17d03607c520c730cc0
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-23 20:01:12 +0000

    Sat, Sep 23, 2023, 9:01 PM +01:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -8,12 +8,12 @@ class shirtificate(FPDF):
     def header(self):
         self.add_page()
         self.image("shirtificate.png", 10, 70, 190)
-        self.set_font("inconsolata", "", 48)
+        self.set_font("courier", "", 48)
         self.cell(0, 57, "CS50 Shirtificate", align="C")
         self.ln(20)
 
     def generate_shirt(self):
-        self.set_font("inconsolata", size=24)
+        self.set_font("courier", size=24)
         self.set_text_color(255, 255, 255)
         self.cell(0, 213, self.name, align="C")
         self.output("shirtificate.pdf")
