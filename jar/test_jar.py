de2c8c5b97e5a494625c30fb58cd99330b2a6930
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-23 17:05:42 +0000

    Sat, Sep 23, 2023, 6:05 PM +01:00

--- jar/test_jar.py
+++ jar/test_jar.py
@@ -1,9 +1,9 @@
 from jar import Jar
-
+from pytest import raises
 
 def test_init():
     jar1 = Jar()
-    assert jar.capacity == 12
+    assert jar1.capacity == 12
     jar2 = Jar(1322980853407936018020929177243811840)
     assert jar2.capacity == 1322980853407936018020929177243811840 # what is the significance of this number?
     jar3 = Jar(0)
@@ -20,8 +20,18 @@ def test_str():
 
 
 def test_deposit():
-    ...
+    jar = Jar()
+    with raises(ValueError):
+        jar.deposit(13)
+    assert jar.size == 0 # whoa there
+    jar.deposit(10)
+    assert jar.size == 10
 
 
 def test_withdraw():
-    ...
+    big_jar = Jar(100)
+    big_jar.deposit(100)
+    with raises(ValueError):
+        big_jar.withdraw(101) # don't be greedy
+    big_jar.withdraw(99) # nom nom nom
+    assert big_jar.size == 1
