6a16068cba75802edb87619838853a2cfccfaf5a
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-23 17:27:24 +0000

    Sat, Sep 23, 2023, 6:27 PM +01:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,4 +1,11 @@
 from fpdf import FPDF
-name = input("Name: ")
 pdf = FPDF()
 pdf.add_page()
+
+
+
+def main():
+    name = input("Name: ").strip() + " took CS50"
+
+if __name__ == "__main__":
+    main()
