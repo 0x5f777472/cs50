4b4048157a1dbbac52cb9c98f9e3709c2452a8b4
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-23 16:59:07 +0000

    Sat, Sep 23, 2023, 5:59 PM +01:00

--- jar/test_jar.py
+++ jar/test_jar.py
@@ -2,9 +2,12 @@ from jar import Jar
 
 
 def test_init():
-    jar = Jar()
-    assert jar().capacity == 12
-    assert jar().capacity == 12
+    jar1 = Jar()
+    assert jar.capacity == 12
+    jar2 = Jar(1322980853407936018020929177243811840)
+    assert jar2.capacity == 1322980853407936018020929177243811840 # what is the significance of this number?
+    jar3 = Jar(0)
+    assert jar3.capacity == 0
 
 
 def test_str():
