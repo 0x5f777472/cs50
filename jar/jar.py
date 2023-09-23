3a292e10545a48b440fb00fed2d8a181233a0f82
0x5f777472 <85249053+0x5f777472@users.noreply.github.com>
2023-09-23 16:37:23 +0000

    Sat, Sep 23, 2023, 5:37 PM +01:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,9 +1,11 @@
 class Jar:
     def __init__(self, capacity=12):
-        ...
+        if capacity < 0:
+            raise ValueError
+        self.capacity = capacity
 
     def __str__(self):
-        ...
+        return "🍪" * n
 
     def deposit(self, n):
         ...
